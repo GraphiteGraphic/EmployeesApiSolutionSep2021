d02c167fedefca27e83259343b79b8dcfdc52553
Jeffry Gonzalez <jeff@hypertheory.com>
2021-09-08 11:08:41 -0400

    done with the demo controller

--- EmployeesApi/Controllers/DemoController.cs
+++ EmployeesApi/Controllers/DemoController.cs
@@ -25,7 +25,7 @@ namespace EmployeesApi.Controllers
         [HttpGet("/orders")] // Query strings only make sens on "queries", which are GET requests on
         // collections. (this means  if you are using query strings on a post or something, you 
         // probably should rethink your design.
-        public ActionResult GetOrders([FromQuery(Name ="sold-by")]string soldBy = "all")
+        public ActionResult GetOrders([FromQuery(Name = "sold-by")] string soldBy = "all")
         {
             return Ok($"Getting you all the orders sold by {soldBy}");
         }
@@ -37,10 +37,26 @@ namespace EmployeesApi.Controllers
         }
 
         [HttpGet("whoami")]
-        public ActionResult GetUserAgent([FromHeader(Name ="User-Agent")] string userAgent)
+        public ActionResult GetUserAgent([FromHeader(Name = "User-Agent")] string userAgent)
         {
             return Ok($"You are telling me you are running {userAgent}");
         }
+
+        [HttpPost("orders")]
+        public ActionResult PlaceAnOrder([FromBody]PostOrderRequest order)
+        {
+            return Ok($"Placing an order for {order.For} for {order.Items.Count} items.");
+        }
+
+
     }
+
+
+    public class PostOrderRequest
+    {
+        public string For { get; set; }
+        public List<int> Items { get; set; }
+    }
+
 }
 
